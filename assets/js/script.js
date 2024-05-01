210994066160194c3cdabad7d8209e9e5f905bfa
João Pedro <joaopedron253@gmail.com>
2024-04-30 21:40:21 -0300

    atualização de informações na seção about, qualifications e home

--- assets/js/script.js
+++ assets/js/script.js
@@ -138,3 +138,37 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
+
+// SCROLL REVEAL
+
+ScrollReveal().reveal('#home', {
+    origin: 'left',
+    duration: 2000,
+    // distance: '20%'
+})
+
+ScrollReveal().reveal('#about', {
+    origin: 'right',
+    duration: 2000,
+    // distance: '20%'
+})
+
+ScrollReveal().reveal('#skills', {
+    origin: 'left',
+    duration: 2000,
+    // distance: '20%'
+})
+
+ScrollReveal().reveal('#qualifications', {
+    origin: 'right',
+    duration: 2000,
+    // distance: '20%'
+})
+
+ScrollReveal().reveal('#contact', {
+    origin: 'left',
+    duration: 2000,
+    // distance: '20%'
+})
